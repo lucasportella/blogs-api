1a8aa38fc48bed50c8719046a4310bc8f2bde973
lucas <lucasportellaagu@gmail.com>
2021-10-01 20:50:59 -0300

    create new user successfully

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
-const app = express();
-
+const bodyParser = require('body-parser');
 const { UsersRoute } = require('../routes');
 
+const app = express();
+app.use(bodyParser.json());
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
--- src/routes/UsersRoute.js
+++ src/routes/UsersRoute.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { User } = require('../sequelize/models');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    res.send('hello!');
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const { displayName, email, password, image } = req.body;
+        const payload = { displayName, email, password, image };
+        const newUser = await User.create(payload);
+        res.status(201).json('usuário criado com sucesso!');
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).json({ message: 'Algo deu errado, verifique o seu terminal' });
+    }
 });
 
 module.exports = router;
