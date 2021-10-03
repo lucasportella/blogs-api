85b1ff5eef8f543f273de631f300c3c731fe7c27
lucas <lucasportellaagu@gmail.com>
2021-10-03 17:13:23 -0300

    validateJWT ready for now, debug decoded later

--- src/middlewares/jwt/validadeJWT.js
+++ src/middlewares/jwt/validadeJWT.js
@@ -2,9 +2,23 @@ const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 require('dotenv').config();
 
+const { User } = require('../../sequelize/models');
+
 const validateJWT = async (req, res, next) => {
     const token = req.headers.authorization;
+    const secret = process.env.JWT_SECRET;
     if (!token) {
         return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token not found' });
     }
+    try {
+        const decoded = jwt.verify(token, secret);
+        console.log(decoded);
+        // DEBUGGAR A PARTIR DAQUI
+    } catch (e) {
+        console.log('erro no validateJWT middleware');
+        console.log(e);
+    }
+    next();
 };
+
+module.exports = validateJWT;
