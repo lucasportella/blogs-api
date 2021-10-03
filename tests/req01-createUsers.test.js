720b0e8e61b0781cd10682a90d24820cc3712d8a
lucas <lucasportellaagu@gmail.com>
2021-10-03 17:40:35 -0300

    fix test token string

--- tests/req01-createUsers.test.js
+++ tests/req01-createUsers.test.js
@@ -22,6 +22,7 @@ describe('1 - Sua aplicação deve ter o endpoint POST `/user`', () => {
       .then((response) => {
         const { json } = response;
         expect(json.token).not.toBeNull();
+        expect(json.token).toMatch(/.*/)
       });
   });
 
