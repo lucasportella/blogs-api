d97c2cb559d4e52253edad10b3096e27774cb5bd
lucas <lucasportellaagu@gmail.com>
2021-10-05 17:08:28 -0300

    catories cannot be edited validatePutBlogPost

--- src/middlewares/validations/validatePutBlogPost.js
+++ src/middlewares/validations/validatePutBlogPost.js
@@ -4,6 +4,7 @@ const validatePutBlogPost = async (req, res, next) => {
 const { error } = Joi.object().keys({
     title: Joi.string().required(),
     content: Joi.string().required(),
+    categoryIds: Joi.forbidden().messages({ 'any.unknown': 'Categories cannot be edited' }),
 }).validate(req.body);
     if (error) {
         return next(error);
