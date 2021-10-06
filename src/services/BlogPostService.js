dfc0e103fcc5303ac519e9c3a564de850410056e
lucas <lucasportellaagu@gmail.com>
2021-10-06 13:39:37 -0300

    refactor postBlogPost to support atomic operation

--- src/services/BlogPostService.js
+++ src/services/BlogPostService.js
@@ -1,39 +1,48 @@
+const Sequelize = require('sequelize');
 const { BlogPost, Category, User, PostsCategory } = require('../sequelize/models');
+const config = require('../sequelize/config/config');
+
+const sequelize = new Sequelize(config.development);
 
 const checkCategoryExistence = async (categoryIds) => {
     const resultArray = await Promise.all(categoryIds.map((id) =>
-    Category.findOne({ where: { id } })));
-    
+        Category.findOne({ where: { id } })));
+
     const checkCategoryIds = resultArray.every((result) => result !== null);
-    
+
     if (!checkCategoryIds) {
         return { errorType: 'not_found', error: { message: '"categoryIds" not found' } };
     }
     return 'all categories exists';
 };
 
+// postBlogPost uses atomic operation
 const postBlogPost = async (payload) => {
-    // REFACTOR WITH SOLID AND ATOMICITY
-    const { title, content, categoryIds, userId } = payload;
-
-    const checkCategories = await checkCategoryExistence(categoryIds);
-
-    if (checkCategories.error) {
-        return checkCategories;
-    }
-
-    const result = await BlogPost.create({ title, content, userId });
-
+    const t = await sequelize.transaction();
+    try {
+        const { title, content, categoryIds, userId } = payload;
+        const checkCategories = await checkCategoryExistence(categoryIds);
+    
+        if (checkCategories.error) {
+            return checkCategories;
+        }
+        const result = await BlogPost.create({ title, content, userId }, { transaction: t });
+    
         await categoryIds.forEach((id) =>
-         PostsCategory.create({ postId: result.id, categoryId: id }));
-
-    return result;
+            PostsCategory.create({ postId: result.id, categoryId: id }, { transaction: t }));
+    
+        await t.commit();
+        return result;
+    } catch (e) {
+        await t.rollback();
+        console.log('ERROR ON ATOMIC OPERATION at postBlogPost service', e);
+    }
 };
 
 const getAllBlogPosts = async () => {
     const result = await BlogPost.findAll({
         include: [{ model: User, as: 'user' },
-         { model: Category, as: 'categories', through: { attributes: [] } }],
+        { model: Category, as: 'categories', through: { attributes: [] } }],
     });
     return result;
 };
@@ -41,8 +50,8 @@ const getAllBlogPosts = async () => {
 const getBlogPost = async (id) => {
     const result = await BlogPost.findOne({
         where: { id },
-include: [{ model: User, as: 'user' },
-         { model: Category, as: 'categories', through: { attributes: [] } }],
+        include: [{ model: User, as: 'user' },
+        { model: Category, as: 'categories', through: { attributes: [] } }],
     });
     if (!result) {
         return { errorType: 'not_found', error: { message: 'Post does not exist' } };
